c0480a5c9607bd93343a73b1d258a74e9383e2c5
xannor <xannor@users.noreply.github.com>
2021-04-24 13:47:42 +0000

    Missed correction

--- custom_components/reolink_dev/utils.py
+++ custom_components/reolink_dev/utils.py
@@ -1,21 +1,29 @@
 """ Utility functions """
 
-from typing import Callable, Dict, Optional, TypeVar
+from typing import Union
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry
+from homeassistant.helpers.device_registry import DeviceEntry, DeviceRegistry
 
 
-TKEY = TypeVar("TKEY")
-TVALUE = TypeVar("TVALUE")
-
-
-def try_get_or_create_item(
-    self: Dict[TKEY, TVALUE], key: TKEY, factory: Callable[[TKEY], Optional[TVALUE]]
+async def async_get_device_entries(
+    hass: HomeAssistant, device: Union[str, DeviceEntry]
 ):
-    """ dict extension to get a value or factory create it """
+    """ Get entires for the device """
 
-    value = self.get(key, None)
-    if not value is None:
-        return value
-    value = factory(key)
-    if not value is None:
-        return self.setdefault(key, value)
-    return None
+    registry = await entity_registry.async_get_registry(hass)
+    if isinstance(device, str):
+        device_registry: DeviceRegistry = (
+            await hass.helpers.device_registry.async_get_registry()
+        )
+        device_entry = device_registry.async_get(device)
+    else:
+        device_entry = device
+
+    entries = (
+        entity_registry.async_entries_for_device(registry, device_entry.id)
+        if device_entry
+        else None
+    )
+
+    return (device_entry, entries)
